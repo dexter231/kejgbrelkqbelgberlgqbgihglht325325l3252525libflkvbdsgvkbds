1a7cd49572701023e144695e8d8c39905e520167
dexter231 <90749199+dexter231@users.noreply.github.com>
2024-04-19 05:00:26 +0530

    Add files via upload

---
+++ llm.py
@@ -0,0 +1,50 @@
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv('GEMINI_API'))
+# Set up the model
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 0,
+  "max_output_tokens": 8192,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
+class gemini:
+    def __init__(self,system_msg):
+        self.history = []
+        self.model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
+                                generation_config=generation_config,
+                                safety_settings=safety_settings,
+                                system_instruction=system_msg)
+        self.convo = self.model.start_chat()
+    def reset(self):
+        self.history.clear()
+
+    def get_response(self, input):
+        self.convo.send_message(input)
+        return self.convo.last.text
+
+
+# llm = gemini("always reply in markdown")
+
+# res=llm.get_response("hello")
+# print(res)
